4fb6c3f78fab84e76f54a19322b29fb201f5334b
tdctran <tdctran@gmail.com>
2016-03-28 23:27:27 +0000

    Deleted duplicated migration file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160323143637) do
+ActiveRecord::Schema.define(version: 20160321141814) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
